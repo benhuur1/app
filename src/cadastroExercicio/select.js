d42c01db3488a583ebe3efceeac7024061141004
benhuur1 <joseben.hur.86@gmail.com>
2021-06-06 17:55:24 -0300

    up

---
+++ src/cadastroExercicio/select.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => {
+  const [select, setSelect] = React.useState('');
+  return (
+    <form>
+      <select
+        value={select}
+        onChange={({ target }) => setSelect(target.value)}
+        id="produtos"
+      >
+        <option disabled value="">Selecione</option>
+        <option value="notebook">Notebook</option>
+        <option value="smartphone">Smartphone</option>
+        <option value="tablet">Tablet</option>
+      </select>
+      <h1>{select}</h1>
+    </form>
+  );
+};
+
+export default App;
